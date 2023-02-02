0c6da6202bbe5ad6a11708df835e1de8bdeee382
stalone01 <124041904+stalone01@users.noreply.github.com>
2023-02-02 11:39:02 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="with=device-with, initial-scale=1.0">
+        <title>Portfolio/Carte de visite</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="ecran">
+            <div class="carte">
+                <img src="avatarfemme.png">
+                <h2>Mampionina Stalone</h2>
+                <p>Developpeur web junior </br>et</br> Developpeur  mobile</p>
+                <div class="reseau">
+                    <a href="https://fr.linkedin.com/"><img src="linkedin.png"></a>   
+                    <a href="https://www.instagram.com/"><img src="tiktok.png"></a>
+                    <a href="https://fr-fr.facebook.com/"><img src="Facebook_f_logo_(2019).svg.png"></a>
+                    <a href="https://twitter.com/"><img src="phone.png"></a> 
+                </div>
+                <a href="mailto:rasoamampioninastalone@gmail.com" class="bouton">Contactez moi</a>
+            </div>
+        </div>
+    </body>
+</html>
